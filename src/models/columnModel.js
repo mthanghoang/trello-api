df16d81bcbd5953ecc0a20e3bdc606376c55cc30
Hoang Minh Thang <mthang.hoang739@gmail.com>
2024-04-10 18:06:23 +0300

    API for updating column

--- src/models/columnModel.js
+++ src/models/columnModel.js
@@ -10,13 +10,16 @@ const COLUMN_COLLECTION_SCHEMA = Joi.object({
 
   // Lưu ý các item trong mảng cardOrderIds là ObjectId nên cần thêm pattern cho chuẩn nhé, (lúc quay video số 57 mình quên nhưng sang đầu video số 58 sẽ có nhắc lại về cái này.)
   cardOrderIds: Joi.array().items(
-    Joi.string().pattern(/^[0-9a-fA-F]{24}$/).message('Your string fails to match the Object Id pattern!')
+    Joi.string().pattern(/^[0-9a-fA-F]{24}$/).message('CardId fails to match the Object Id pattern!')
   ).default([]),
 
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroyed: Joi.boolean().default(false)
 })
+
+// Chỉ định những Fields ko cho phép cập nhật
+const INVALID_UPDATE_FIELDS = ['_id', 'boardId', 'createdAt']
 
 const validateBeforeCreate = async (data) => {
   return await COLUMN_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
@@ -59,10 +62,28 @@ const createNew = async (data) => {
   }
 }
 
+const update = async (columnId, dataToUpdate) => {
+  try {
+    Object.keys(dataToUpdate).forEach(fieldName => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete dataToUpdate[fieldName]
+      }
+    })
+    return await GET_DB().collection(COLUMN_COLLECTION_NAME).findOneAndUpdate(
+      { _id: new ObjectId(columnId), _destroyed: false },
+      { $set: dataToUpdate },
+      { returnDocument: 'after' }
+    )
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 export const columnModel = {
   COLUMN_COLLECTION_NAME,
   COLUMN_COLLECTION_SCHEMA,
   createNew,
   findOneById,
-  pushCardOrderIds
+  pushCardOrderIds,
+  update
 }
--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -23,6 +23,30 @@ const createNew = async (req, res, next) => {
 
 }
 
+const update = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    // Nếu làm thêm tính năng chuyển column sang board khác thì mới validate boardId
+    // boardId: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).message('BoardId fails to match the Object Id pattern!'),
+    title: Joi.string().min(3).max(50).trim().strict(),
+    cardOrderIds: Joi.array().items(
+      Joi.string().pattern(/^[0-9a-fA-F]{24}$/).message('CardId fails to match the Object Id pattern!')
+    ).default([])
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true //cho phép đẩy lên các field chưa biết (vd trong case này là columnOrderIds)
+    })
+    next()
+  } catch (error) {
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY,
+      new Error(error).message
+    ))
+  }
+}
+
 export const columnValidation = {
-  createNew
+  createNew,
+  update
 }
